6bc89ee01b10bbc18e11c797643380a66dac97f0
Juan Pablo Pabón <juanpapabon@gmail.com>
2022-02-07 16:42:24 -0500

    refactor: add strict typing in function and method declarations in migrations

--- database/migrations/2022_02_07_070000_products.php
+++ database/migrations/2022_02_07_070000_products.php
@@ -11,7 +11,7 @@ class Products extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('product_id');
@@ -28,7 +28,7 @@ class Products extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         //
     }
